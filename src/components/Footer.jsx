4eaa5acaf4fd9fde93ee6ec0f2b14a5e209e4690
unknown <sarwadeepdhaval@gmail.com>
2025-09-29 13:54:04 +0530

    UI only

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center text-gray-600 py-4">
-      <p className="text-sm">© 2025 Smart Tourist Safety. All rights reserved.</p>
-    </footer>
-  );
-}
